c7a7fcc046975d68e960126b752ff6413e4ed279
vikramsengar <vikram.sengar@affinityeducation.in>
2024-01-29 00:46:56 +0530

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Image from 'next/image'
 import styles from "./home.module.css"
+import Link from 'next/link'
 
 export default function Home() {
   return <div>
     <div className={styles.container}>
       <div className={styles.textContainer}>
-        <h1 className={styles.title}>Empower Minds, Transforming Notes – Your Gateway to Educational Excellence!</h1>
+        <h1 className={styles.title}>Welcome to <p className={styles.word}>BIOXCELERATE</p> - Your Ultimate Learning Hub!</h1>
         <p className={styles.desc}>
         Empower your education journey with our premium notes. Elevate your learning experience effortlessly. Your trusted source for high-quality study materials, designed to guide you towards academic excellence and success.
         </p>
@@ -32,9 +33,26 @@ export default function Home() {
         <Image src="/homeimg.gif" alt="" fill className={styles.heroImg}/>
       </div>
     </div>
-    {/* <div>
-      <div><h4>SOURCE</h4></div>
-    </div> */}
+    <div>
+      <div className={styles.headertag}><h4>Our services</h4></div>
+      <div className={styles.sub_container}>
+        <div className={styles.sub_container_box}>
+          <h3>Notes</h3>
+          <p>Lessons on design that cover the most recent developments.</p>
+          <Link href="/">Learn More</Link>
+        </div>
+        <div className={styles.sub_container_box}>
+          <h3>Guidance</h3>
+          <p>Lessons on design that cover the most recent developments.</p>
+          <Link href="/">Learn More</Link>
+        </div>
+        <div className={styles.sub_container_box}>
+          <h3>Blogs</h3>
+          <p>Lessons on design that cover the most recent developments.</p>
+          <Link href="/">Learn More</Link>
+        </div>
+      </div>
+    </div>
 
   </div>
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,9 +3,9 @@ import styles from "./footer.module.css";
 const Footer = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.logo}>lamadev</div>
+      <div className={styles.logo}>BIOXCELERATE</div>
       <div className={styles.text}>
-        Lama creative thoughts agency © All rights reserved.
+        BIOXCELERATE  © All rights reserved.
       </div>
     </div>
   )
